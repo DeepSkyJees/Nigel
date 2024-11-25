69d0134892269c59983970ffcdb3c80d1de44cf6
简一思 <secondgalaxy@163.com>
2024-11-25 15:56:50 +0800

    feature#Json MsgPack

--- Source/Nigel.Basic/ObjectExtension.cs
+++ Source/Nigel.Basic/ObjectExtension.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using System;
 using System.Text.Json;
 using Nigel.Basic.JsonConverters;
+using MessagePack;
 
 namespace Nigel.Basic
 {
@@ -15,7 +16,7 @@ namespace Nigel.Basic
         /// <param name="obj">The object.</param>
         /// <param name="jsonConverters">The json converters.</param>
         /// <returns></returns>
-        public static string ToJson(this object obj, JsonConverter[]? jsonConverters = null)
+        public static string ToJson(this object obj, JsonConverter[] jsonConverters)
         {
             var setting = new JsonSerializerSettings
             {
@@ -29,18 +30,29 @@ namespace Nigel.Basic
             return JsonConvert.SerializeObject(obj, setting);
         }
 
-        ///// <summary>
-        ///// CamelCasePropertyNamesContractResolver
-        ///// </summary>
-        ///// <param name="obj">The object.</param>
-        ///// <returns></returns>
-        //public static string ToJson(this object obj)
-        //{
-        //    var setting = new JsonSerializerSettings
-        //    {
-        //        ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver(),
-        //    };
-        //    return JsonConvert.SerializeObject(obj, setting);
-        //}
+        public static string ToMsgPackJson(this object obj, MessagePackSerializerOptions options = null)
+        {
+
+            if (options == null)
+            {
+                options = MessagePack.Resolvers.ContractlessStandardResolver.Options;
+            }
+            var blob = MessagePackSerializer.Serialize(obj, options);
+            return MessagePackSerializer.ConvertToJson(blob, options);
+        }
+
+        /// <summary>
+        /// CamelCasePropertyNamesContractResolver
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns></returns>
+        public static string ToJson(this object obj)
+        {
+            var setting = new JsonSerializerSettings
+            {
+                ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver(),
+            };
+            return JsonConvert.SerializeObject(obj, setting);
+        }
     }
 }
