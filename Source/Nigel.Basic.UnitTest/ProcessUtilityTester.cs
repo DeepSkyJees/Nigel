4ee0d86dd8a4039f11a0bcdbbf64af492ad26583
DeepSkyJees <secondgalaxy@163.com>
2026-01-04 11:46:54 +0800

    feature#添加ProcessUtility

---
+++ Source/Nigel.Basic.UnitTest/ProcessUtilityTester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nigel.Basic.Utility;
+using Xunit;
+
+namespace Nigel.Basic.UnitTest
+{
+    public class ProcessUtilityTester
+    {
+        [Fact]
+        public async Task GetCodeProcessId()
+        {
+            var id = ProcessUtility.GetProcessIdByName("Code.exe");
+            Assert.NotNull(id);
+        }
+    }
+}
---
+++ Source/Nigel.Basic/Utility/ProcessUtility.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nigel.Basic.Utility
+{
+    public class ProcessUtility
+    {
+        /// <summary>
+        /// 获取指定名称的进程ID
+        /// </summary>
+        /// <param name="processName"></param>
+        /// <returns></returns>
+        public static int GetProcessIdByName(string processName)
+        {
+            // 如果传入的是带.exe的名称，可以移除扩展名
+            processName = Path.GetFileNameWithoutExtension(processName);
+
+            var processes = Process.GetProcessesByName(processName);
+
+            if (processes.Any())
+            {
+                // 返回第一个找到的进程ID
+                return processes.FirstOrDefault().Id;
+            }
+
+            return -1; // 未找到进程
+        }
+
+
+        /// <summary>
+        /// 获取指定名称的进程ID集合
+        /// </summary>
+        /// <param name="processName"></param>
+        /// <returns></returns>
+        public static List<int> GetProcessIdsByName(string processName)
+        {
+            // 如果传入的是带.exe的名称，可以移除扩展名
+            processName = Path.GetFileNameWithoutExtension(processName);
+
+            var processes = Process.GetProcessesByName(processName);
+
+            if (processes.Any())
+            {
+                return processes.Select(p => p.Id).ToList();
+            }
+            return null; // 未找到进程
+        }
+    }
+}
